811506714accb21f87239e5ed2e92fd1fac59c08
viktorTsikalovskiy <viktor.tsikalovskiy@gmail.com>
2024-02-15 18:11:20 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,6 @@ export const App = () => {
     <div
       style={{
         height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
       }}
     >
       React homework template
